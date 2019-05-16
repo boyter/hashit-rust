19f4f7269cf50f0d1ae804b6797c9de5ac61e73c
Ben Boyter <ben@boyter.org>
2019-05-16 15:30:20 +1000

    Displaying output of file input

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,24 @@ use md5::{Md5, Digest};
 use clap::{App, Arg};
 
 fn main() {
-    let _matches = App::new("hashit")
+    let matches = App::new("hashit")
         .version("1.0")
         .about("Hash files in many hashing algorithms")
         .author("Ben Boyter")
-        .arg(Arg::with_name("INPUT")
+        .arg(Arg::with_name("FILES")
             .help("Sets the input file(s) to hash")
             .required(true)
             .multiple(true)
             .index(1))
         .get_matches();
+
+
+    // Safe to unwrap as INPUT is a required filed
+    let dir_names: Vec<&str> = matches.values_of("FILES").unwrap().collect();
+
+    for (_size, &s) in dir_names.iter().enumerate() {
+        println!("{}", s);
+    }
 
     let mut hasher = Md5::new();
     let data = b"Hello world!";
